2aacd49ab0d45c3e44d947ba64a238c4645e6194
Rishie <rishie@rishie.se>
2023-12-18 23:13:56 +0100

    Added URLs

--- docker-host.sh
+++ docker-host.sh
@@ -5,8 +5,8 @@ service docker start
 
 unifi_folder='unifi-network-application'
 mkdir -p "/etc/$unifi_folder/mongo/"
-wget -O "/etc/$unifi_folder/mongo/init-mongo.js"
+wget -O "/etc/$unifi_folder/mongo/init-mongo.js" https://raw.githubusercontent.com/fishie/hemmait/main/unifi/init-mongo.js
 mkdir "$unifi_folder"
-wget -O "$unifi_folder/compose.yml"
+wget -O "$unifi_folder/compose.yml" https://raw.githubusercontent.com/fishie/hemmait/main/unifi/compose.yml
 cd "$unifi_folder"
 docker compose up -d
