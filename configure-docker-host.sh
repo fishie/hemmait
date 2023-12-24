f996322c83549831bdd04a015451941fff134795
Rishie <rishie@rishie.se>
2023-12-24 11:30:52 +0100

    Use pwgen instead

--- configure-docker-host.sh
+++ configure-docker-host.sh
@@ -1,11 +1,11 @@
 apk update
-apk add docker docker-compose micro
+apk add docker docker-compose micro pwgen
 rc-update add docker
 service docker start
 
 unifi_folder='unifi-network-application'
 
-export MONGO_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+export MONGO_PASS=$(pwgen 12 1)
 mkdir -p "/etc/$unifi_folder/mongo/"
 wget -O "/etc/$unifi_folder/mongo/init-mongo.js" https://raw.githubusercontent.com/fishie/hemmait/main/unifi/init-mongo.js
 sed -i "s/\$MONGO_PASS/$MONGO_PASS/g" "/etc/$unifi_folder/mongo/init-mongo.js"
