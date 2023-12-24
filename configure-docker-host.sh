8427f715d96ff6b2ef579a39ee5ddf7baae22cbb
Rishie <rishie@rishie.se>
2023-12-24 12:40:07 +0100

    Small change and clean code

--- configure-docker-host.sh
+++ configure-docker-host.sh
@@ -4,14 +4,15 @@ rc-update add docker
 service docker start
 
 unifi_folder='unifi-network-application'
+repo_url='https://raw.githubusercontent.com/fishie/hemmait/main/unifi'
 
-export MONGO_PASS=$(pwgen 12 1)
+export MONGO_PASS=$(pwgen -s)
 mkdir -p "/etc/$unifi_folder/mongo/"
-wget -O "/etc/$unifi_folder/mongo/init-mongo.js" https://raw.githubusercontent.com/fishie/hemmait/main/unifi/init-mongo.js
+wget -O "/etc/$unifi_folder/mongo/init-mongo.js" "$repo_url/init-mongo.js"
 sed -i "s/\$MONGO_PASS/$MONGO_PASS/g" "/etc/$unifi_folder/mongo/init-mongo.js"
 
 mkdir "$unifi_folder"
-wget -O "$unifi_folder/compose.yml" https://raw.githubusercontent.com/fishie/hemmait/main/unifi/compose.yml
+wget -O "$unifi_folder/compose.yml" "$repo_url/compose.yml"
 cd "$unifi_folder"
 
 while ! docker info > /dev/null 2>&1; do
